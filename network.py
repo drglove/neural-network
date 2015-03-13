b89ffa189b232847d38f5df2e5af8459cc16fb73
Nicholas Lange <nick.t.lange@gmail.com>
2015-03-13 15:32:52 -0700

    Implemented backpropagation

--- network.py
+++ network.py
@@ -1,20 +1,23 @@
 import numpy as np
+from itertools import izip_longest
 
 class Network:
-    def __init__(self, learn_rate=1):
+    def __init__(self):
         self.layers = {'input' : None, 'output' : None, 'hidden' : []}
-        self.learn_rate = learn_rate
-    def connect(self, layer_from, layer_to, from_is_input=False, to_is_output=False):
-        layer_from.connect(layer_to)
-        if from_is_input:
+    def connect(self, layer_from, layer_to, weights=[[]], from_is_input=False, to_is_output=False):
+        layer_from.connect(layer_to, weights)
+        if from_is_input and to_is_output:
             self.layers['input'] = layer_from
-        else:
-            self.layers['hidden'].append(layer_from)
-
-        if to_is_output:
             self.layers['output'] = layer_to
-        else:
+        elif from_is_input and not to_is_output:
+            self.layers['input'] = layer_from
             self.layers['hidden'].append(layer_to)
+        elif not from_is_input and not to_is_output:
+            self.layers['hidden'].append(layer_to)
+            self.layers['hidden'].append(layer_from)
+        elif not from_is_input and to_is_output:
+            self.layers['hidden'].append(layer_from)
+            self.layers['output'] = layer_to
     def set_input(self, input_values):
         if self.layers['input']:
             self.layers['input'].set_input(input_values)
@@ -22,29 +25,37 @@ class Network:
             print 'No input layer set on the network!'
     def output(self):
         return self.layers['output'].output()
-    def train(self, input_values, truth_values):
+    def train(self, input_values, truth_values, learn_rate=1):
         self.set_input(input_values)
-        # TODO: Calculate change in weights from backpropagation
+        # Calculate change in weights from backpropagation
         # Do the output layer first and work backwards
-        o = np.array(self.layers['output'].output())
         t = np.array(truth_values)
-        d = (o - t) * o * (1 - o)
-        x = self.layers['output'].prev_layer.output()
-        for neuron in self.layers['output'].prev_layer.neurons:
-            # do things
-        # Need dE/dw_(ij) so need xi
+        layer = self.layers['output']
+        while layer.prev_layer and self.layers['input'] != layer.prev_layer:
+            layer = layer.prev_layer
+            o = np.array(layer.output())
+            weights_out = np.array([n.weighted_outputs.values() for n in layer.neurons])
+            if layer != self.layers['output']:
+                d = (o - t)
+            else:
+                d = weights_out.dot(d)
+            d = d * (o * (1 - o))
+            for neuron_to, delta_w in zip(layer.neurons, d):
+                for neuron_from in neuron_to.weighted_inputs.keys():
+                    x = neuron_from.output()
+                    neuron_from.update_weight(neuron_to, learn_rate*delta_w*x, incremental=True)
 
 class NetworkLayer:
     def __init__(self, n_neurons=1):
         self.neurons = [Neuron() for _ in xrange(n_neurons)]
         self.next_layer = None
-    def connect(self, layer_to):
+        self.prev_layer = None
+    def connect(self, layer_to, weights=[[]]):
         self.next_layer = layer_to
         layer_to.prev_layer = self
-        for neuron_from in self.neurons:
-            for neuron_to in layer_to.neurons:
-                neuron_from.set_output(neuron_to)
-                neuron_to.add_input(neuron_from)
+        for neuron_to, input_weights in izip_longest(layer_to.neurons, weights):
+            for neuron_from, weight in izip_longest(self.neurons, input_weights):
+                neuron_from.add_output(neuron_to, weight)
     def set_input(self, input_values):
         for i in xrange(len(self.neurons)):
             self.neurons[i].set_input(input_values[i])
@@ -52,11 +63,23 @@ class NetworkLayer:
         return [neuron.output() for neuron in self.neurons]
 
 class Neuron:
-    def __init__(self, input_value=None):
+    def __init__(self, name=None, input_value=None):
+        if not name:
+            self.name = Neuron.get_next_name()
         self.weighted_inputs = {}
+        self.weighted_outputs = {}
         self.input_value = input_value
-        self.output_neuron = None
-        self.bias = 0.1
+        self.bias = 0.0
+    def __str__(self):
+        return self.name
+    def __repr__(self):
+        return str(self)
+    name = 'a'
+    @staticmethod
+    def get_next_name():
+        curr = Neuron.name
+        Neuron.name = chr(ord(Neuron.name) + 1)
+        return curr
     def output(self):
         if self.input_value:
             y = self.input_value
@@ -69,10 +92,20 @@ class Neuron:
         if not weight:
             weight = np.random.rand()
         self.weighted_inputs[neuron] = weight
+    def add_output(self, neuron, weight=None):
+        if not weight:
+            weight = np.random.rand()
+        self.weighted_outputs[neuron] = weight
+        neuron.add_input(self, weight)
+    def update_weight(self, neuron_to, weight, incremental=False):
+        if not incremental:
+            self.weighted_outputs[neuron_to] = weight
+            neuron_to.weighted_inputs[self] = weight
+        else:
+            self.weighted_outputs[neuron_to] += weight
+            neuron_to.weighted_inputs[self] += weight
     def set_input(self, input_value):
         self.input_value = input_value
-    def set_output(self, neuron):
-        self.output_neuron = neuron
 
 def sigmoid(x):
     return 1.0 / (1.0 + np.exp(-x))
--- test2.py
+++ test2.py
@@ -1,9 +1,11 @@
 from network import *
+# example from 3.1 of below
+# https://www4.rgu.ac.uk/files/chapter3%20-%20bp.pdf
 net = Network()
-layer_in = NetworkLayer(3)
-layer_out = NetworkLayer(2)
-layer_hidden = NetworkLayer(6)
-net.connect(layer_in, layer_hidden, from_is_input=True)
-net.connect(layer_hidden, layer_out, to_is_output=True)
-net.train([1, 2, 555], [3, -1])
+layer_in = NetworkLayer(2)
+layer_hidden = NetworkLayer(2)
+layer_out = NetworkLayer(1)
+net.connect(layer_in, layer_hidden, weights=[[0.1, 0.8], [0.4, 0.6]], from_is_input=True)
+net.connect(layer_hidden, layer_out, weights=[[0.3, 0.9]], to_is_output=True)
+net.train([0.35, 0.9], [0.5])
 print net.output()
